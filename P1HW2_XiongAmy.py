2c8bd8ff2b87a7083dd33a123dbb5b4c6b8722c1
Amy Xiong <101990402+amyxg@users.noreply.github.com>
2024-02-14 11:09:57 -0500

    updated

--- P1HW2_XiongAmy.py
+++ P1HW2_XiongAmy.py
@@ -5,19 +5,19 @@
 
 print("This program calculates and displays travel expenses\n")
 
-budget = int(input('Enter Budget: '))
+budget = float(input('Enter Budget: $'))
 location = str(input('Enter your travel destination: '))
-fuel = int(input('How much do you think on will spend on gas? '))
-accomondation = int(input('Approximately, how much will you need for accomodation/hotel? '))
-food = int(input('Last, how much do you need for food? '))
+fuel = float(input('How much do you think on will spend on gas? $'))
+accomondation = float(input('Approximately, how much will you need for accomodation/hotel? $'))
+food = float(input('Last, how much do you need for food? $'))
 
 print("")
 print("-------Travel Expenses-------")
 print('Location:', location)
-print('Initial Budget:', budget, '\n')
-print('Fuel:', fuel)
-print('Accomodation:', accomondation)
-print('Food:', food, '\n')
+print('Initial Budget: $', budget, '\n')
+print('Fuel: $', fuel)
+print('Accomodation: $', accomondation)
+print('Food: $', food, '\n')
 
 remainderBal = budget - (fuel + accomondation + food)
-print('Remaining Balance:', remainderBal)
+print('Remaining Balance: $', remainderBal)
